3d36b85f31888dd4c1bf421e4b9ac6a8107076bd
Nikita Semin <30572809+fortsev@users.noreply.github.com>
2025-02-22 12:41:40 +0300

    update app.vue options > composition

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import {ref, computed} from "vue";
 import AppHeader from "@/components/main/AppHeader.vue";
 import AppFilters from "@/components/AppFilters.vue";
 import AppTodoList from "@/components/todo/AppTodoList.vue";
@@ -9,76 +9,63 @@ import {Todo} from "@/types/todo";
 import {Filter} from "@/types/Filters";
 import {Stats} from "@/types/Stats";
 
-interface State {
-  todos: Todo[],
-  activeFilter: Filter,
-}
-export default defineComponent({
-  components: {
-    AppHeader,
-    AppFilters,
-    AppTodoList,
-    AppAddTodo,
-    AppFooter
-  },
-  data(): State {
-    return {
-      todos: [
-        { id: 0, text: 'hello', completed: true },
-        { id: 1, text: 'world', completed: false },
-        { id: 2, text: 'vue', completed: false },
-      ],
-      activeFilter: 'All',
-    }
-  },
-  computed: {
-    activeTodos():Todo[] {
-      return this.todos.filter(el => !el.completed)
-    },
-    doneTodos(): Todo[] {
-      return this.todos.filter(el => el.completed)
-    },
-    filterTodos():Todo[] {
-      if (this.activeFilter === 'Active') return this.activeTodos
-      if (this.activeFilter === 'Done') return this.doneTodos
-      return this.todos
-    },
-    stats(): Stats {
-      return {
-        active: this.activeTodos.length,
-        done: this.doneTodos.length
-      }
-    }
-  },
-  methods: {
-    toggleTodo(id: number) {
-      const targetTodo = this.todos.find((todo: Todo) => todo.id === id)
+const todos = ref<Todo[]>([
+  {id: 0, text: 'hello', completed: true},
+  {id: 1, text: 'world', completed: false},
+  {id: 2, text: 'vue', completed: false},
+])
+const activeFilter = ref<Filter>('All')
 
-      if (targetTodo) {
-        targetTodo.completed = !targetTodo.completed
-      }
-    },
-    removeTodo(id: number) {
-      this.todos = this.todos.filter((todo: Todo) => todo.id !== id)
-    },
-    addTodo(todo: Todo) {
-      this.todos.push(todo)
-    },
-    changeActiveFilter(filter: Filter) {
-      this.activeFilter = filter
-    }
+const activeTodos = computed<Todo[]>(() => {
+  return todos.value.filter(el => !el.completed)
+})
+
+const doneTodos = computed<Todo[]>(() => {
+  return todos.value.filter(el => el.completed)
+})
+
+const filterTodos = computed<Todo[]>(() => {
+  if (activeFilter.value === 'Active') return activeTodos.value;
+  if (activeFilter.value === 'Done') return doneTodos.value;
+  return todos.value
+})
+
+const stats = computed<Stats>(() => {
+  return {
+    active: activeTodos.value.length,
+    done: doneTodos.value.length
   }
 })
+
+const toggleTodo = (id: number) => {
+  const targetTodo = todos.value.find((todo: Todo) => todo.id === id)
+
+  if (targetTodo) {
+    targetTodo.completed = !targetTodo.completed
+  }
+}
+
+const removeTodo = (id: number) => {
+  todos.value = todos.value.filter((todo: Todo) => todo.id !== id)
+}
+
+const addTodo = (todo: Todo) => {
+  todos.value.push(todo)
+}
+
+const changeActiveFilter = (filter: Filter) => {
+  activeFilter.value = filter
+}
 </script>
 
 <template>
-  <app-header />
+  <app-header/>
   <app-filters :active-filter="activeFilter" @changeActiveFilter="changeActiveFilter"/>
   <main class="app-main">
-    <app-todo-list :todos="filterTodos" @toggleTodo="toggleTodo" @removeTodo="removeTodo" />
-    <app-add-todo @addTodo="addTodo" />
+    <app-todo-list :todos="filterTodos" @toggleTodo="toggleTodo" @removeTodo="removeTodo"/>
+    <app-add-todo @addTodo="addTodo"/>
   </main>
-  <app-footer :stats="stats" />
+  <app-footer :stats="stats"/>
 </template>
 
 <style>
